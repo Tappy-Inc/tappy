cb2847c2e8cce90d005a04206948b599741044d9
Gelo <chrisgelosulit@gmail.com>
2023-07-28 23:20:20 +0800

    Added husky config

--- src/env.mjs
+++ src/env.mjs
@@ -9,12 +9,14 @@ export const env = createEnv({
   isServer: typeof window === "undefined",
   server: {
     TEST: z.string().min(1),
+    NODE_ENV: z.enum(["development", "production"]),
   },
   client: {
     NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production"]),
   },
   runtimeEnv: {
     TEST: process.env.TEST,
+    NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
   },
 });
