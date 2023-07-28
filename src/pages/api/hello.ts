69189b9d0497d7755cdfba0d075e69efbaf7e57d
Gelo <chrisgelosulit@gmail.com>
2023-07-29 00:49:55 +0800

    Test api endpoint using @vercel/postgres

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { sql } from "@vercel/postgres";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { rows } = await sql`SELECT * FROM users`;
+
+  res.status(200).json({ rows });
 }
