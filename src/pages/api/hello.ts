28027375c138c38b0fe4282ad622e0091a367a22
Gelo <chrisgelosulit@gmail.com>
2023-08-07 15:12:07 +0800

    Remove vercel lib

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,12 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { sql } from "@vercel/postgres";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { rows } = await sql`SELECT * FROM users`;
+  await Promise.resolve();
 
-  res.status(200).json({ rows });
+  res.status(200).json({ rows: [] });
 }
